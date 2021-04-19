f5f4f12d5fd19ba68cb7ee363ff1241f4031e7ff
shardul1991 <82703427+shardul1991@users.noreply.github.com>
2021-04-19 17:24:19 +0530

    By Git Team
    Added sum method

--- GIT_DEMO/src/com/cjc/deo1.java
+++ GIT_DEMO/src/com/cjc/deo1.java
@@ -4,5 +4,10 @@ public class deo1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello shardul");
+		
+		public void sum()
+		{
+			System.out.println("Sum....");
+		}	
 	}
 }
